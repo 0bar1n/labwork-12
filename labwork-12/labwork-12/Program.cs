3201f8185fb96abd0d6ad15ac5167ce4dcd5fed1
barinskaya <114239444+barinskaya@users.noreply.github.com>
2022-11-27 21:25:42 +0300

    Add files via upload

---
+++ labwork-12/labwork-12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace labwork_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k; //переменная целого типа
+            double s = 0;//переменная вещественного типа
+            double fact = 1; //переменная вещественного типа
+            Console.WriteLine("Введите количество элементов последовательности"); //вывод на консоль
+            k = Convert.ToInt32(Console.ReadLine());//ввод и задание переменной
+            for (int i = 1; i <= k; i++) //цикл, пока выполняется условие
+            {
+                fact = fact * i; //факториал
+                s += 1 / fact; //сумма элементов последовательности
+            }
+            Console.WriteLine(s); //вывод результата
+        }
+    }
+}
